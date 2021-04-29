66348b7472219c0f51a4bc1db071c3a5f1fbf071
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-04-29 16:51:54 +0900

    メニューバーへの表示

--- app/javascript/pulldown.js
+++ app/javascript/pulldown.js
@@ -2,6 +2,9 @@ window.addEventListener("load", function() {
 
   const pullDownButton = document.getElementById("lists");
   const pullDownParents = document.getElementById("pull-down");
+  // querySelectorAllメソッドでHTML要素を取得し、配列で格納
+  const pullDownChild = document.querySelectorAll(".pull-down-list");
+  const currentList = document.getElementById("current-list");
 
   pullDownButton.addEventListener("mouseover", function() {
     this.setAttribute("style", "background-color: red;")
@@ -12,12 +15,19 @@ window.addEventListener("load", function() {
   });
 
   pullDownButton.addEventListener("click", function() {
-    
+    // 条件分岐で表示非表示を分ける
     if (pullDownParents.getAttribute("style") == "display: block;") {
       pullDownParents.removeAttribute("style");
     } else {
       pullDownParents.setAttribute("style", "display: block;")
     };
+  });
+
+  pullDownChild.forEach(function(list) {
+    list.addEventListener("click", function() {
+      value = list.innerHTML;  // innerHTMLで文字列を取得
+      currentList.innerHTML = value;  // innerHTMLでHTML要素を書き換え
+    });
   });
 });
 
